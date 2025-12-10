a2440938514d9737a02b994b05e3e56d243b7ee4
xlc-dev <119954023+xlc-dev@users.noreply.github.com>
2025-12-10 13:49:51 +0100

    setup solidjs and vite

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <div style={{ padding: "2rem", "font-family": "system-ui, sans-serif" }}>
+      <h1>Auwebbity</h1>
+      <p>Simple Audacity alternative for the web.</p>
+    </div>
+  );
+};
+
+export default App;
